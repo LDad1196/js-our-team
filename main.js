706a7548b769a0fda11ad4fc01ad545e6ca5d2a4
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-05-07 14:37:50 +0200

    m2 completata

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // Aggiungere a mano i vari elementi dell'array
 let ceo = {nome: "Wayne Barnett", ruolo: "Founder & CEO", immagine: "wayne-barnett-founder-ceo.jpg"}
 
@@ -16,6 +17,22 @@ let team =[ceo, chief, office, social, developer, graphic]
 console.log(team)
 
 for (let index = 0; index < team.length; index++) {
+    
     const elemento = team[index];
+    
     console.log( team[index].nome, team[index].ruolo)
+
+    printPage(team.nome, team.ruolo, team.immagine)
 }
+
+function printPage (nome, ruolo, immagine) {
+        teamBox.innerHTML += 
+        `<div class="card text-center p-2 my-2">
+                <figure>
+                    <img src="./img/${team.immagine}" class="w-25"></img>
+                </figure>
+                <h2>${team.nome}</h2> 
+                <h3>${team.ruolo}</h3>
+                </div>`
+}
+
